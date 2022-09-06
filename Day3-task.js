7d2a8fce01253eec17eed0de012de4a617befdf8
IbrahimEmpire <110913184+IbrahimEmpire@users.noreply.github.com>
2022-09-06 10:42:23 +0530

    Add files via upload

---
+++ Day3-task.js
@@ -0,0 +1,19 @@
+var Resume = {
+    
+    Name : "O. Mohamed Ibrahim",
+    Age : 24,
+    Email : "ibu8189914744@gmail.com",
+    Mobile : 8189914744,
+    Address : "10/4/172 , Nakkeeran Street, Paramakudi",
+    Qualification : "Diploma in Mechanical Engineering",
+    
+         Course1 : "Auto Cadd, Creo , Solid Work",
+         Course2 : "Full Stack Development",   
+         Course3 : " CNC Milling & Programming",  
+   TechnicalSkills : "HTML,css,javascript",
+   LanguagesKnown : "English,Tamil,Malay"
+
+
+}
+
+console.log(JSON.stringify(Resume));
